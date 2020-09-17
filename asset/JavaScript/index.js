e7260a4e2d10e9f64b54b2aa032861746a02bb1a
unknown <jonmakesitbetter@gmail.com>
2020-09-17 13:48:00 -0400

    Added inspirational quote generator and appended to aside div

--- asset/JavaScript/index.js
+++ asset/JavaScript/index.js
@@ -1,2 +1,36 @@
-console.log("Hello World"); 
-alert("Our JS is working");
+var CORS = "https://cors-anywhere.herokuapp.com/"
+
+var submitBtn = document.getElementById("input-btn");
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: "https://api.forismatic.com/api/1.0/",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        data: {
+          method: "getQuote",
+          lang: "en",
+          format: "jsonp"
+        }
+      }).then(function(response) {
+        var quote = response.quoteText;
+        var author = response.quoteAuthor;
+        var quoteDiv = $("<div>").text(quote);
+        var authorDiv = $("<div>").text(author);
+        $("#company-section").append(quoteDiv);
+        $("#company-section").append(authorDiv);
+        
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
