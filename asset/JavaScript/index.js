557414ae941c518022a7ef9095eb6b1d2e6031cc
unknown <jonmakesitbetter@gmail.com>
2020-09-17 14:43:34 -0400

    Refactored js for inspirational quotes

--- asset/JavaScript/index.js
+++ asset/JavaScript/index.js
@@ -1,36 +1,34 @@
-var CORS = "https://cors-anywhere.herokuapp.com/"
+$(document).ready( function(){
 
-var submitBtn = document.getElementById("input-btn");
-
-submitBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    $.ajax({
-        url: "https://api.forismatic.com/api/1.0/",
-        jsonp: "jsonp",
-        dataType: "jsonp",
-        data: {
-          method: "getQuote",
-          lang: "en",
-          format: "jsonp"
-        }
-      }).then(function(response) {
-        var quote = response.quoteText;
-        var author = response.quoteAuthor;
-        var quoteDiv = $("<div>").text(quote);
-        var authorDiv = $("<div>").text(author);
-        $("#random-generator").append(quoteDiv);
-        $("#random-generator").append(authorDiv);
-        
-      });
-});
-
-
-
-
-
-
-
-
-
-
-
+    var CORS = "https://cors-anywhere.herokuapp.com/"
+    
+    var submitBtn = $("#input-btn");
+    var firstContainer = $("#first-container");
+    var secondContainer = $("#second-container");
+    
+    submitBtn.on("click", function(event) {
+        event.preventDefault();
+        firstContainer.addClass("hide");
+        secondContainer.removeClass("hide");
+        $.ajax({
+            url: "https://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+              method: "getQuote",
+              lang: "en",
+              format: "jsonp"
+            }
+          }).then(function(response) {
+            var quote = response.quoteText;
+            var author = response.quoteAuthor;
+            var quoteDiv = $("<div>").text(quote);
+            var authorDiv = $("<div>").text(author);
+            $("#random-generator").append(quoteDiv);
+            $("#random-generator").append(authorDiv);
+            
+          });
+    });
+    
+    })
+    
